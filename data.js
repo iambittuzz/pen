ac5c0bd9b81d05398feeaf19fb095119bc9e7f28
iambittuzz <89698917+iambittuzz@users.noreply.github.com>
2022-03-13 08:05:05 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,29 @@
+module.exports = {
+
+    mongoLink:'',//database link
+    
+    bot_token:'5282006556:AAF_F-ZLWxQzhb0bxtlD4xFTvtx0tYiCRBM',//bot token
+    
+    bot_name:'COINBASEdemobot',//bot username without @
+    
+    bot_admin:65555,// admin telegram id
+    
+    channelsList : ['@Bsclooters'],// channels list
+    
+    reffer_bonus:0.1,// refferal bonus amount
+    
+    min_wd:0.1,//min withdrawal amount
+    
+    daily_bonus:0.1,// daily bonus amount
+    
+    currency:'DGB', //bot cuurency
+    
+    cb_api_key:'ecec5f339bc8e157053ea1fd19bad74f7a5fdb2ff1769584242719094b8efbcb',//coinbase api key
+    
+    cb_api_secret:'5f4E6fa6C8E66d582de2aAf68DF81202e3f47Df1F8ce26c60d147b18981E524B',// coinbase secret key
+    
+    cb_account_id:'',// counbase account id for the bot currency
+    
+    payment_channel:'@UniversePayouts'// payment channel username
+    
+    }
